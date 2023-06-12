8cb46ad50c4626d315167321c72b99de076b252e
noot-noot-pengu <random_dude_233@proton.me>
2023-06-12 23:30:48 +0100

    bencode research

---
+++ src/bencode/decode.rs
@@ -0,0 +1,49 @@
+fn get_string() {
+    
+}
+
+fn get_int() {
+    
+}
+
+fn get_list() {
+    
+}
+
+fn get_dict() {
+    
+}
+
+
+pub fn decode() {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,52 @@ use std::io::prelude::*;
 
 
 
+
+
+mod bencode;
+
+
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
 fn main() -> std::io::Result<()> {
-    //let mut file = File::open("test.torrent")?;
-    //let mut contents = String::new();
-    //file.read_to_string(&mut contents)?;
+    //let path = "test.torrent";
+    //let mut file = File::open(path)?;
+    //let mut buf = vec![];
+    //file.read_to_end (&mut buf)?;
+    //let contents = String::from_utf8_lossy (&buf);
     //println!("{:?}", contents);
+    
+    
+    
+    
+    
+    
+    let point = Point { x: 1, y: 2 };
 
+    // Convert the Point to a JSON string.
+    let serialized = serde_json::to_string(&point).unwrap();
 
-    //println!("{:?}", file.file_name());
-    let path = "test.torrent";
+    // Prints serialized = {"x":1,"y":2}
+    println!("serialized = {}", serialized);
 
-    let mut file = File::open(path)?;
-    let mut buf = vec![];
-    file.read_to_end (&mut buf)?;
-    let contents = String::from_utf8_lossy (&buf);
-    println!("{:?}", contents);
+    // Convert the JSON string back to a Point.
+    let deserialized: Point = serde_json::from_str(&serialized).unwrap();
+
+    // Prints deserialized = Point { x: 1, y: 2 }
+    println!("deserialized = {:?}", deserialized);
+    
+    
+    
+    
+    
+    
+    
+    
     Ok(())
 }
