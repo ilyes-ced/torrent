78f5d4635314c0a0df9adc7e8122b63687f85ccd
ilyes-ced <random_dude_233@proton.me>
2025-02-12 14:40:47 +0000

    loging
    Former-commit-id: 9c3023b92d2369857d3c4ded056c5d95c85b6ea2 [formerly f0ad99a2f3f9434ecdb4146eb397b078aa26826b]
    Former-commit-id: fef861f95fe410df876890fc0b19b28a09026e41

--- src/log.rs
+++ src/log.rs
@@ -3,17 +3,53 @@ use chrono::{
     Datelike, Timelike,
 };
 
+enum Color {
+    Red,
+    Green,
+    Yellow,
+    Blue,
+}
+
 pub fn info(msg: String) {
-    println!("\033[93mError\033[0m");
+    println!(
+        "{} {} {}",
+        time_date(),
+        color("[ INFO ]".to_string(), Color::Blue),
+        color(msg, Color::Blue),
+    );
 }
 pub fn debug(msg: String) {
-    println!("\033[93mError\033[0m");
+    println!(
+        "{} {} {}",
+        time_date(),
+        color("[ DEBUG ]".to_string(), Color::Green),
+        color(msg, Color::Green),
+    );
 }
 pub fn warning(msg: String) {
-    println!("\033[93mError\033[0m");
+    println!(
+        "{} {} {}",
+        time_date(),
+        color("[ WARNING ]".to_string(), Color::Yellow),
+        color(msg, Color::Yellow),
+    );
 }
 pub fn error(msg: String) {
-    println!("\033[93mError\033[0m");
+    println!(
+        "{} {} {}",
+        time_date(),
+        color("[ ERROR ]".to_string(), Color::Red),
+        color(msg, Color::Red),
+    );
+}
+
+fn color(txt: String, color: Color) -> String {
+    match color {
+        Color::Red => return format!("\x1b[31m{}\x1b[0m", txt),
+        Color::Green => return format!("\x1b[32m{}\x1b[0m", txt),
+        Color::Yellow => return format!("\x1b[33m{}\x1b[0m", txt),
+        Color::Blue => return format!("\x1b[34m{}\x1b[0m", txt),
+    }
 }
 
 fn time_date() -> String {
